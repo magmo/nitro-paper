7939c23d7f41d0ffc6af384a57ec350fffff76bc
Tom Close <tom.close@cantab.net>
2019-01-03 11:03:45 -0800

    restrict -> cap. more writing

--- code/claim.py
+++ code/claim.py
@@ -1,16 +1,14 @@
 from overlap import overlap
 from remove import remove
-from restrict import restrict
+from cap import cap
 
 MAX = 2**256
 
 def claim(recipient, guarantee, outcome, funding, amount):
-  potentialPayout = overlap(recipient, outcome, MAX)
-  # todo: need to 'fully restrict' the guarantee
-  restrictedGuarantee = restrict(guarantee, recipient, potentialPayout)
-  if overlap(recipient, restrictedGuarantee, funding) >= amount:
+  cappedGuarantee = cap(guarantee, outcome)
+  if overlap(recipient, cappedGuarantee, funding) >= amount:
     funding = funding - amount
-    guarantee = remove(restrictedGuarantee, recipient, amount)
+    guarantee = remove(cappedGuarantee, recipient, amount)
     outcome = remove(outcome, recipient, amount)
   else:
     amount = 0
--- code/tests.py
+++ code/tests.py
@@ -1,6 +1,6 @@
 from overlap import overlap
 from remove import remove
-from restrict import restrict
+from cap import cap
 from transfer import transfer
 from claim import claim
 
@@ -18,10 +18,10 @@ def test_transfer():
   assert transfer('a', [['a', 5]], 10, 2) == (2, [['a', 3]], 8)
   assert transfer('b', [['a', 5]], 10, 2) == (0, [['a', 5]], 10)
 
-def test_restrict():
-  assert restrict([['a', 5], ['b', 3]], 'a', 4) == [['a', 4], ['b', 3]]
-  assert restrict([['a', 5], ['b', 3]], 'a', 6) == [['a', 5], ['b', 3]]
-  assert restrict([['a', 5], ['b', 3]], 'b', 2) == [['a', 5], ['b', 2]]
+def test_cap():
+  assert cap([['a', 5], ['b', 3]], [['a', 4]]) == [['a', 4], ['b', 0]]
+  assert cap([['a', 5], ['b', 3]], [['a', 6]]) == [['a', 5], ['b', 0]]
+  assert cap([['a', 5], ['b', 3]], [['b', 2]]) == [['a', 0], ['b', 2]]
 
 def test_claim():
   assert claim('a', [['a', 10]], [['a', 5]], 5, 5) == (5, [['a', 0]], [['a', 0]], 0)
